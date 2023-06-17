9f3507a317198bb9170cd92977d74872e63ab288
ZerokHypeR <zerokhyper1047@gmail.com>
2023-06-17 00:07:00 -0500

    zzz Z

--- ceropajas.py
+++ ceropajas.py
@@ -1,10 +1,29 @@
-def suma (a, b):
-    resultado = a + b
-    return resultado
+class Persona:
+    def __init__ (self,nombre,apellido):
+        self.nombre = nombre 
+        self.apellido = apellido
 
-def resta (a, b):
-    resultado = a - b
-    return resultado
+    def hablar(self):
+        return "hablando",+"hola"
+
+    def caminar (self):
+        return "caminando"
+
+    def imprimirDatos(self):
+        return f"Nombre: {self.nombre} {self.apellido}"
+
+
+Persona = Persona("fg", "zerok")
+
+mensajeHablar = Persona.hablar()
+mensajeCaminar = Persona.caminar()
+datosPersona = Persona.imprimirDatos()
+
+print(mensajeHablar)
+print(mensajeCaminar)
+print(datosPersona)
+
+
 
 
 
